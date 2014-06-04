5bf1c74b813f5fe8d21c255e30a7f652a58c8db7
Jan Szewczak <jan.szewczak@implix.com>
2014-06-05 01:03:41 +0200

    my youtube

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,27 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
+  'ngCookies',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+]);
+
+myApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/listoffilms', {
+                templateUrl: 'partials/listoffilms.html',
+                controller: 'ListOfFilms'
+            }).
+            when('/film/:id', {
+                templateUrl: 'partials/film.html',
+                controller: 'Film'
+            }).
+            otherwise({
+                redirectTo: '/listoffilms'
+            });
+    }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,38 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+var controlers = angular.module('myApp.controllers', []);
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+controlers.controller('ListOfFilms', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get('json/myfilms.json').success(function(data) {
+            $scope.films = data;
+        });
 
-  }]);
+        $scope.orderProp = 'name';
+    }]);
+
+controlers.controller('Film', ['$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http) {
+        $http.get('json/films/' + $routeParams.id + '.json').success(function(data) {
+            $scope.film = data;
+            $scope.code = data.yt_id;
+        });
+
+    }]);
+
+controlers.directive('myYoutube', function($sce) {
+    return {
+        restrict: 'EA',
+        scope: { code:'=' },
+        replace: true,
+        template: '<div style="height:400px;"><iframe style="overflow:hidden;height:390px;width:640px" width="640px" height="390px" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
+        link: function (scope) {
+            scope.$watch('code', function (newVal) {
+                if (newVal) {
+                    scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal + "?autoplay=1");
+                }
+            });
+        }
+    };
+});
